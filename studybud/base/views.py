a563fcde636513f57613f71323b3ed93d7bfd962
Sudarshan04 <sudarshanrajan2017@gmail.com>
2024-05-10 00:20:29 +0530

    form for crud operation

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
+from .models import Room
 # Create your views here.
-rooms = [
-    {'id':1, 'name':'Lets learn python'},
-    {'id':2, 'name':'frontend'},
-    {'id':3, 'name':'design'},
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn python'},
+#     {'id':2, 'name':'frontend'},
+#     {'id':3, 'name':'design'},
+# ]
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == (pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request,'base/room.html',context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
